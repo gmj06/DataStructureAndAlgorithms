34e15496d61033c6329272c9e40bad9f058d7447
GMJ06 <gowrimj2006@gmail.com>
2019-07-30 01:20:49 -0700

    Modified the code as per reviewer's feedback

--- RoutePlanner/student_code.py
+++ RoutePlanner/student_code.py
@@ -18,10 +18,11 @@ def reconstruct_path(came_from, current, start):
     total_path = []
     
     while current != start:
-        total_path.insert(0, current)
+        total_path.append(current)
         current = came_from[current]
        
-    total_path.insert(0, start)    
+    total_path.append(start)
+    total_path.reverse()
     return total_path
 
 
@@ -36,25 +37,29 @@ def shortest_path(M, start, goal):
     came_from[start] = None # For the start intersection, there won't be any previous intersection. Hence, set as None
     g_score[start] = 0
     heapq.heappush(open_set, (0, start)) # adding the start intersection to the Priority Queue
+    visited_set = set()
     
     
     while len(open_set) > 0:
         current = heapq.heappop(open_set)[1]  # Fetching the intersect with lowest cost (g_score) from the Priority Queue
         
-        if current == goal:
-            return reconstruct_path(came_from, current, start)
-            
-        
-        for neighbor in M.roads[current]:
-            # f(i) = g(i) + h(i) for intersect i
-            tentative_g_score = g_score[current] + Euclidean_distance(M.intersections[current], M.intersections[neighbor])
-            
-            if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
-                came_from[neighbor] = current
-                g_score[neighbor] = tentative_g_score
-                
-                if neighbor not in open_set:
-                    heapq.heappush(open_set, (tentative_g_score, neighbor)) 
+        if current in visited_set:
+            continue
+        else:
+            if current == goal:
+                return reconstruct_path(came_from, current, start)
+
+
+            for neighbor in M.roads[current]:
+                # f(i) = g(i) + h(i) for intersect i
+                tentative_g_score = g_score[current] + Euclidean_distance(M.intersections[current], M.intersections[neighbor])
+
+                if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                    came_from[neighbor] = current
+                    g_score[neighbor] = tentative_g_score
+
+                    if neighbor not in visited_set:
+                        heapq.heappush(open_set, (tentative_g_score, neighbor)) 
                     
     return 
             
